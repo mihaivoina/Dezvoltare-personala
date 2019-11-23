ed2cdbb85520993f7ae047872690e1f98e9478a7
marian-potor <marian.potor@yahoo.com>
2019-11-23 16:30:44 +0200

    Logic is done; UI in progres

--- src/questions.js
+++ src/questions.js
@@ -1,27 +1,29 @@
 import React from 'react';
-import Axios from 'axios'
+import Axios from 'axios';
+import { ToggleButtonGroup, ToggleButton, Carousel } from 'react-bootstrap';
+import './questions.css'
 
 class Questions extends React.Component {
     state = {
-        numberOfQuestions: 10,
-        topicList: null,
-        randomQuestionList: []
+        numberOfQuestions: 4,
+        // topicList: null,
+        randomQuestionList: [],
+        results: null
     }
 
 
     async getQuestions () {
         const res = await Axios('http://localhost:3002/questions');
-        // create question list
         const questions = res.data;
-         // create list with all question topics:
-        const allTopics = questions.map(q => q.topic);
-        // filter topics and create list with individual topics :
-        const topicList = allTopics.reduce((topics, el) => {
-            if (!topics.includes(el)) {
-                topics.push(el);
-            }
-            return topics;
-        },[]);
+        //  // create list with all question topics:
+        // const allTopics = questions.map(q => q.topic);
+        // // filter topics and create list with individual topics :
+        // const topicList = allTopics.reduce((topics, el) => {
+        //     if (!topics.includes(el)) {
+        //         topics.push(el);
+        //     }
+        //     return topics;
+        // },[]);
         // create array with 10(can be how many you) random numbers coresponding to the question list:
         // get a list of questions coresponding to the random numbers:
         const randomQuestionList = [];
@@ -29,9 +31,12 @@ class Questions extends React.Component {
         for (let num of random) {
             randomQuestionList.push(questions[num])
         }
+        for (let question of randomQuestionList) {
+            question.score = "";
+        }
 
         this.setState({
-            topicList,
+            // topicList,
             randomQuestionList
         });
     }
@@ -49,36 +54,125 @@ class Questions extends React.Component {
     }
 
     //method for creating radio buttons for every question:
+
+    // getRadio (obj) {
+    //     const radio = [];
+    //     for (let answer in obj.answers) {
+    //         let inputKey = obj.question.concat(answer)
+    //         let labelKey = answer.concat(obj.question)
+    //         radio.push(<input 
+    //             type="radio" 
+    //             name={ obj.question } 
+    //             id={ obj.answers.answer } 
+    //             value={ answer } 
+    //             key={ inputKey } 
+    //             checked={ this.state.randomQuestionList.question } 
+    //             onChange={ this.handleChange }/>)
+    //         radio.push(<label htmlFor= { obj.answers.answer } key={ labelKey }> { obj.answers[answer] }  </label>)
+    //     }
+    //     return radio;
+    // }
+
     getRadio (obj) {
         const radio = [];
         for (let answer in obj.answers) {
-            let inputKey = obj.question.concat(obj.answers[answer])
-            let labelKey = obj.question.concat(answer)
-            radio.push(<input type="radio" name={ obj.question } id={ obj.answers.answer } value={ answer } key={ inputKey } />)
-            radio.push(<label htmlFor= { obj.answers.answer } key={ labelKey }> { obj.answers[answer] }  </label>)
+            let inputKey = obj.question.concat(answer);
+            radio.push(<ToggleButton
+                variant='primary'
+                name={ obj.question } 
+                id={ obj.answers.answer } 
+                value={ answer } 
+                key={ inputKey } 
+                checked={ this.state.randomQuestionList.question } 
+                onChange={ this.handleChange }>
+                    { obj.answers[answer] }
+                </ToggleButton>);
         }
         return radio;
     }
 
-    handleClick () {
+    handleChange = e => {
+        const value = e.target.value;
+        const question = e.target.name;
+        const randomQuestionList = this.state.randomQuestionList;
+        randomQuestionList.map(oldq => {
+            if (oldq.question === question) {
+                oldq.score = value;
+            }
+            return oldq;
+        })
+        this.setState({
+            randomQuestionList
+        })
+    }
 
+    handleClick = () => {
+        const finalAnswers = this.state.randomQuestionList;
+        if (finalAnswers.find(el => el.score === '')) {
+            alert('Please answer all the questions')
+            return;
+        }
+        // const topicList = this.state.topicList;
+        // create list with all question topics:
+        const allTopics = finalAnswers.map(q => q.topic);
+        // filter topics and create list with individual topics :
+        const topicList = allTopics.reduce((topics, el) => {
+            if (!topics.includes(el)) {
+                topics.push(el);
+            }
+            return topics;
+        },[]);
+        let results = [];
+        for (let topic of topicList) {
+            results.push({topic,
+                          score: 0,
+                          numOfQuestions: 0,
+                          average: 0
+                        })
+        }
+        for (let result of results) {
+            finalAnswers.map(el => {
+                if (el.topic === result.topic) {
+                    result.numOfQuestions++;
+                    result.score += Number(el.score);
+                }
+                return result; 
+            })
+        }
+        for (let result of results) {
+            result.average = result.score/result.numOfQuestions;
+        }
+        console.log(results);
+        
     }
 
     componentDidMount () {
         this.getQuestions();
     }
+
     render () {
         return(
-            <ul>
+            <>
+            <Carousel interval={ null } wrap={ false }>
                 { this.state.randomQuestionList.map(el => (
-                    <li key={ el.question }>
-                        <p>{ el.message }</p>
-                        <p>{ el.question }</p>
-                        { this.getRadio(el) }
-                    </li>)) }
-                <button onClick={ this.handleClick }></button>
-            </ul>
+                    <Carousel.Item key={ el.question }>
+                        <img className="d-block w-100" src='Black.png' alt='Background' />
+                        <Carousel.Caption key={ el.question }>
+                            {/* <p>{ el.message }</p> */}
+                            <p>{ el.question }</p>
+                            <ToggleButtonGroup type='radio' name={ el.question }>
+                            { this.getRadio(el) }
+                            </ToggleButtonGroup>
+                        </Carousel.Caption>
+                    </Carousel.Item>)) }
+            </Carousel>
+            <button onClick={ this.handleClick }>Submit</button>
+          </>
         )
     }
 };
 export default Questions;
+
+
+
+// value = [ {"topic":"life", "score":"15"}]
